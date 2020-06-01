9644359be5a242bc54bc43f45550a2f737a17b91
Valentí Gàmez <vgr.gamez@gmail.com>
2020-06-01 09:36:53 +0200

    Updated language to spanish.

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Tu password ha sido reseteado!',
+    'sent' => 'Hemos enviado un correo con el link para resetear tu password!',
+    'throttled' => 'Espere antes de reintentarlo porfavor.',
+    'token' => 'El token para resetear este password no es válido.',
+    'user' => "No podemos encontrar el usuario asociado a esa cuenta de correo.",
+
+];
