7c8462937d0efd0db72bcf21eccfaed3c74df577
Valentí Gàmez <vgr.gamez@gmail.com>
2020-06-03 15:45:58 +0200

    Catched error database not found.

--- app/Modules/Base/Traits/CustomerDB.php
+++ app/Modules/Base/Traits/CustomerDB.php
@@ -3,6 +3,7 @@
 namespace App\Modules\Base\Traits;
 
 use App\Modules\User\Domain\User;
+use Facade\FlareClient\Http\Exceptions\NotFound;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 
@@ -11,13 +12,17 @@ trait CustomerDB
 
     public function setToCustomerDB(User $user): void
     {
-        Config::set('database.connections.tenant.host', $user->db_host);
-        Config::set('database.connections.tenant.username', $user->db_user);
-        Config::set('database.connections.tenant.password', $user->db_password);
-        Config::set('database.connections.tenant.database', $user->db_name);
+        try {
+            Config::set('database.connections.tenant.host', $user->db_host);
+            Config::set('database.connections.tenant.username', $user->db_user);
+            Config::set('database.connections.tenant.password', $user->db_password);
+            Config::set('database.connections.tenant.database', $user->db_name);
 
-        //If you want to use query builder without having to specify the connection
-        Config::set('database.default', 'tenant');
-        DB::reconnect('tenant');
+            //If you want to use query builder without having to specify the connection
+            Config::set('database.default', 'tenant');
+            DB::reconnect('tenant');
+        } catch (\Exception $e) {
+            throw new NotFound();
+        }
     }
 }
